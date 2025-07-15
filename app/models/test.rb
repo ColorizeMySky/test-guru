75c87c901e59fb2cf7b216f6e2e4615b415d3980
Elena <elena.liga@insales.ru>
2025-07-15 23:19:33 +0300

    feat: seeds and 2 methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.sorted_names_by_category(category_name)
+    joins(:category)
+      .where(categories: { name: category_name })
+      .order(name: :desc)
+      .pluck(:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# db/seeds.rb
+# Создание пользователей
+user1 = User.create!(email: 'user1@example.com', password: 'password1')
+user2 = User.create!(email: 'user2@example.com', password: 'password2')
+
+# Создание категорий
+category1 = Category.create!(name: 'Бэкенд')
+category2 = Category.create!(name: 'Фронтенд')
+
+# Создание тестов
+test1 = Test.create!(name: 'Основы Ruby', level: 1, category: category1, author: user1)
+test2 = Test.create!(name: 'Основы HTML', level: 1, category: category2, author: user2)
+
+# Создание вопросов
+question1 = Question.create!(text: 'Что такое Ruby?', score: 5, test: test1)
+question2 = Question.create!(text: 'Что такое HTML?', score: 5, test: test2)
+
+# Создание ответов
+Answer.create!(answer_text: 'Язык программирования', is_correct: true, question: question1)
+Answer.create!(answer_text: 'Язык разметки', is_correct: true, question: question2)
