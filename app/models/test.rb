3204e293f88e7f161ef4af541d6d82b46f35b3c0
Elena <elena.liga@insales.ru>
2025-07-17 14:48:50 +0300

    feat: methods without associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
   def self.sorted_names_by_category(category_name)
-    joins(:category)
-      .where(categories: { name: category_name })
+    joins('JOIN categories ON categories.id = tests.category_id')
+      .where('categories.name = ?', category_name)
       .order(name: :desc)
       .pluck(:name)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   def tests_by_level(level)
-    tests.where(level: level)
+    Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
+        .where('user_tests.user_id = ? AND tests.level = ?', id, level)
+        .distinct
   end
 end
