5d9dd8a2c21e0eaf75ff56475def30ecd4b9f747
Elena <elena.liga@insales.ru>
2025-07-22 18:26:59 +0300

    feat: add author as association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   has_many :results
   has_many :tests, through: :results
+  has_many :authored_tests, foreign_key: 'author_id', class_name: 'Test'
 
   def tests_by_level(level)
     Test.joins("JOIN results ON results.test_id = tests.id")
