54529457a141c111215c40c7dc470ac58f028341
Elena <elena.liga@insales.ru>
2025-07-22 17:11:37 +0300

    feat: add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
+
   def self.sorted_names_by_category(category_name)
     joins('JOIN categories ON categories.id = tests.category_id')
       .where('categories.name = ?', category_name)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :results
+  has_many :tests, through: :results
+
   def tests_by_level(level)
     Test.joins("JOIN results ON results.test_id = tests.id")
         .where("results.user_id = ? AND tests.level = ?", self.id, level)
