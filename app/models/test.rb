090eb4c64493fd8cbec1c123e4af7decbbb1e7e9
Elena <elena.liga@insales.ru>
2025-07-24 00:11:43 +0300

    fix: after review

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,13 +3,14 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
-  has_many :questions
-  has_many :results
+
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
   has_many :users, through: :results
 
   def self.sorted_names_by_category(category_name)
-    joins('JOIN categories ON categories.id = tests.category_id')
-      .where('categories.name = ?', category_name)
+    joins(:category)
+      .where(categories: { name: category_name })
       .order(name: :desc)
       .pluck(:name)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :results
+  has_many :results, dependent: :destroy
   has_many :tests, through: :results
-  has_many :authored_tests, foreign_key: 'author_id', class_name: 'Test'
+  has_many :authored_tests, foreign_key: 'author_id', class_name: 'Test', dependent: :restrict_with_error
 
   def tests_by_level(level)
-    Test.joins("JOIN results ON results.test_id = tests.id")
-        .where("results.user_id = ? AND tests.level = ?", self.id, level)
+    tests.where(level: level)
   end
 end
