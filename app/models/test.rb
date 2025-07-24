0c7e17b6b3bafe82f615ad931bc42c4fbfce7718
ColorizeMySky <41089247+ColorizeMySky@users.noreply.github.com>
2025-07-24 21:04:11 +0300

    Задание к уроку "Ассоциации моделей" (#4)
    * fix: conflict during rebase
    
    * refactor: remove db/development from git
    
    * fix: conflict during rebase
    
    * refactor: delete unnessussery fields from Result model
    
    * feat: add associations
    
    * feat: add author as association
    
    * fix: after review
    
    ---------
    
    Co-authored-by: Elena <elena.liga@insales.ru>

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User'
+
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
+  has_many :users, through: :results
+
   def self.sorted_names_by_category(category_name)
-    joins('JOIN categories ON categories.id = tests.category_id')
-      .where('categories.name = ?', category_name)
+    joins(:category)
+      .where(categories: { name: category_name })
       .order(name: :desc)
       .pluck(:name)
   end
