c97dcab0f8bdad6fca2ac83ade5d1cb4946579ed
Elena <elena.liga@insales.ru>
2025-08-13 00:52:06 +0300

    feat: add test_passages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :results, dependent: :destroy
-  has_many :tests, through: :results
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
   has_many :authored_tests, foreign_key: 'author_id', class_name: 'Test', dependent: :restrict_with_error
 
   scope :tests_by_level, ->(level) { joins(:results).where(tests: { level: level }) }
 
   validates :email, presence: true
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
 end
