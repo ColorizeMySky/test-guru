b98129b70f0cf1a8318aa27cf83b21824af59644
Elena <elena.liga@insales.ru>
2025-08-28 19:53:08 +0300

    style: refactoring of flash

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 module FlashHelper
-  def flash_messages
-    flash.to_hash
+  def render_flash
+    safe_join(
+      flash.map do |type, message|
+        next if type == 'timedout'
+        content_tag :div, message, class: "alert alert-#{flash_class(type)} alert-dismissible fade show", role: 'alert'
+      end.compact,
+      ' '.html_safe
+    )
+  end
+
+  private
+
+  def flash_class(type)
+    case type
+    when 'notice' then 'success'
+    when 'alert' then 'danger'
+    when 'warning' then 'warning'
+    when 'info'    then 'info'
+    else 'secondary'
+    end
   end
 end
