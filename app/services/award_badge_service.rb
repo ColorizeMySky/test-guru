d461de5271f95ac0f0abcdce52cd95ec1ccbd4e4
Your Name <you@example.com>
2025-09-14 22:08:32 +0300

    fix: rubocop error

--- app/services/award_badge_service.rb
+++ app/services/award_badge_service.rb
@@ -12,7 +12,7 @@ class AwardBadgeService
       rule_type = badge.rule_type
       value = badge.value
 
-      UserBadge.create!(user: user, badge: badge) if BadgeRule.applies_to?(user, test, score, rule_type)
+      UserBadge.create!(user: user, badge: badge) if BadgeRule.applies_to?(user, test, score, rule_type, value)
     end
   end
 end
