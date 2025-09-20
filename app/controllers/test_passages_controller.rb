3c933f273f9619a0908191ee3b8d811295a2c7e8
Your Name <you@example.com>
2025-09-21 00:24:24 +0300

    fix: after review

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -11,12 +11,12 @@ class TestPassagesController < ApplicationController
   def update
     @test_passage.accept!(params[:answer_ids])
 
-    return render :show unless @test_passage.completed?
-
-    if @test_passage.expired_by_timer
-      redirect_to result_test_passage_path(@test_passage), alert: 'Время вышло. Тест завершён.'
+    if @test_passage.completed?
+      AwardBadgeService.call(@test_passage)
+      send_completion_email
+      redirect_to result_test_passage_path(@test_passage)
     else
-      handle_completed_test
+      render :show
     end
   end
 
@@ -30,11 +30,5 @@ class TestPassagesController < ApplicationController
     TestsMailer.completed_test(@test_passage).deliver_now
   rescue Net::SMTPAuthenticationError, Net::SMTPError, IOError => e
     Rails.logger.error "Failed to send email: #{e.message}"
-  end
-
-  def handle_completed_test
-    AwardBadgeService.call(@test_passage)
-    send_completion_email
-    redirect_to result_test_passage_path(@test_passage)
   end
 end
